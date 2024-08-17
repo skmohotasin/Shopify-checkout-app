6c071dfd7d63eb14247189bea9fdd22a30bdd3cf
Skeikh Shohan <sk.shohan@live.com>
2024-08-18 05:54:54 +0600

    add folder

---
+++ checkout-visual-tools/extensions/show-compare-at-price/src/Checkout.jsx
@@ -0,0 +1,63 @@
+import {
+  reactExtension,
+  Banner,
+  BlockStack,
+  Checkbox,
+  Text,
+  useApi,
+  useApplyAttributeChange,
+  useInstructions,
+  useTranslate,
+  useSettings,
+} from "@shopify/ui-extensions-react/checkout";
+
+// 1. Choose an extension target
+export default reactExtension("purchase.checkout.block.render", () => (
+  <Extension />
+));
+
+function Extension() {
+  const translate = useTranslate();
+  const { extension, shop } = useApi();
+  const instructions = useInstructions();
+  const applyAttributeChange = useApplyAttributeChange();
+  const {banner_title} = useSettings();
+
+  // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
+  if (!instructions.attributes.canUpdateAttributes) {
+    return (
+      <Banner title="Show compare at price" status="warning">
+        {translate("attributeChangesAreNotSupported")}
+      </Banner>
+    );
+  }
+
+  // 3. Render a UI
+  console.log("pintu",extension)
+  return (
+    <BlockStack border={"dotted"} padding={"tight"}>
+      <Text>{banner_title}</Text>
+      <Text>
+      {shop.name}
+      </Text>
+      <Banner title="Show compare at price">
+        {translate("welcome", {
+          target: <Text emphasis="italic">{extension.target}</Text>,
+        })}
+      </Banner>
+      <Checkbox onChange={onCheckboxChange}>
+        {translate("iWouldLikeAFreeGiftWithMyOrder")}
+      </Checkbox>
+    </BlockStack>
+  );
+
+  async function onCheckboxChange(isChecked) {
+    // 4. Call the API to modify checkout
+    const result = await applyAttributeChange({
+      key: "requestedFreeGift",
+      type: "updateAttribute",
+      value: isChecked ? "yes" : "no",
+    });
+    console.log("applyAttributeChange result", result);
+  }
+}
